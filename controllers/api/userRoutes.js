729d9981f9d3da5a6754ee7918b38994a5d0d0c4
shumaela101~ <shumu.laeeq@gmail.com>
2024-03-27 17:22:48 -0400

    edits for logout and editpost routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -61,5 +61,6 @@ router.post('/logout', (req, res) => {
   }
 });
 
+
 module.exports = router;
 
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,22 +1,26 @@
-const logout = async () => {
-    try {
-      const response = await fetch('/api/users/logout', {
-        method: 'POST',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to logout');
-      }
-    } catch (error) {
-      console.error('Error logging out:', error);
-    }
-  };
-  
-  // Example usage:
-  // Attach this function to a logout button in my HTML
-  // <button onclick="logout()">Logout</button>
+const logout = async (event) => {
+  event.preventDefault();
+  // const username = document.getElementById('username').value;
+  // const password = document.getElementById('password').value;
+  try {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+    });
 
-  document.getElementById('logout').addEventListener('submit', logout)
-  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to logout');
+    }
+  } catch (error) {
+    console.error('Error logging out:', error);
+  }
+};
+
+
+document.getElementById('logout').addEventListener('submit', logout)
+
+
+// Example usage:
+// Attach this function to a logout button in my HTML
+// <button onclick="logout()">Logout</button>
